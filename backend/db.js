a20424828c47dd1044ea3bc27cf096b72119299f
Diya Goyal <goyaldiya67@gmail.com>
2024-07-25 20:38:25 +0530

    dded sql2 pool statements

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'Fashion_feed'
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'Fashion_feed',
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the MySQL database.');
+pool.getConnection((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database.');
 });
 
-module.exports = connection;
+module.exports = pool;
